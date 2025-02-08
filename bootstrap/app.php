a9ea4709177afac338dfc942c2137bcc7eadce9f
Aulia153 <auliasilmi133@gmail.com>
2025-02-08 23:03:19 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+        'check.route' => \App\Http\Middleware\CheckRouteName::class, 
+        'check.user' => \App\Http\Middleware\CheckUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
